7dcc8f4ae17bf7a2a08827094ace9d4cb7bcebd5
Bidyut Sikder <89535239+Bidyut-Sikder@users.noreply.github.com>
2024-09-16 15:50:27 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-let arr = [9, 3, 12, 5, 4, 22, 7];
 
-function insertionSort(array) {
-  for (let i = 1; i < array.length; i++) {
-    let currentValue = array[i];
-    for (var j = i - 1; j >= 0 && array[j] > currentValue; j--) {
-      array[j + 1] = array[j];
-    }
-    array[j + 1] = currentValue;
-  }
-
-  return array;
-}
-
-console.log(insertionSort(arr));
