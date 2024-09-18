d03c5b740aa91db072d14f3d77fb99443be9c6a3
Bidyut-Sikder <bidyutsikder2001@gmail.com>
2024-09-18 12:13:00 +0600

    adding singly linked list push,traverse

--- index.js
+++ index.js
@@ -1,3 +1,57 @@
-//merge sort
 
 
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class singlyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  traverse() {
+    //*traverse means itarating through all elements
+    let current = this.head;
+    while (current) {
+      current = current.next;
+    }
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+  
+  
+  
+  
+  }
+}
+
+let list = new singlyLinkedList();
+
+list.push("bidyut");
+list.push("hii");
+list.push("hiii");
+list.push("hifii");
+list.push("hidii");
+
+list.traverse();
