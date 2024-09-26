debf2a45a2fcccf8d353f67ddb36c770e10366d3
Bidyut-Sikder <bidyutsikder2001@gmail.com>
2024-09-26 11:20:24 +0600

    extracting maximum value of a binary heap

--- index.js
+++ index.js
@@ -20,9 +20,57 @@ class MaxBinaryHeap {
       elementIndex = parentIndex;
     }
   }
+
+  extractMax() {
+    let max = this.values[0];
+    let end = this.values.pop();
+    this.values[0] = end;
+
+    this.sinkDown();
+    console.log("fd");
+    console.log(this.values);
+    return max;
+  }
+  sinkDown() {
+    let inx = 0;
+    let element = this.values[0];
+    let length = this.values.length;
+    let leftChild;
+    let rightChild;
+
+    while (true) {
+      let leftChildIndex = 2 * inx + 1;
+      let rightChildIndex = 2 * inx + 2;
+      let swap = null;
+
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild > element) {
+          swap = leftChildIndex;
+        }
+      }
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+
+        if (
+          (swap === null && element < rightChild) ||
+          (swap !== null && leftChild < rightChild)
+        ) {
+          swap = rightChildIndex;
+        }
+      }
+
+      if (swap === null) break;
+
+      this.values[inx] = this.values[swap];
+      this.values[swap] = element;
+      inx = swap;
+    }
+  }
 }
 
 let heap = new MaxBinaryHeap();
-heap.insert(200);
+//heap.insert(200);
 //heap.insert(16);
+heap.extractMax();
 console.log(heap);
